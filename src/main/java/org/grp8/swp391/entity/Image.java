a1dc9fdd3ed98819c266b6773c329eee1cf6fb52
Unkwonss <thinh769090@example.com>
2025-09-16 11:33:11 +0700

    update code in thinh_beo

---
+++ src/main/java/org/grp8/swp391/entity/Image.java
@@ -0,0 +1,20 @@
+package org.grp8.swp391.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imageId;
+    @ManyToOne
+    @JoinColumn (name="listing_Id",nullable=false)
+    private Listing listing_Id;
+    private String url;
+}
