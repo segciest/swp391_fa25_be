294f9c41ee49c7f0900e0f0af806367db10c9fb5
khoahd14805 <huynhdangkhoaj7@gmail.com>
2025-09-17 19:14:27 +0700

    update code them moi 2 entity

---
+++ src/main/java/org/grp8/swp391/entity/Subscription.java
@@ -0,0 +1,23 @@
+package org.grp8.swp391.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String Sub_id;
+    private String Sub_name;
+    private String Sub_details;
+    private String Sub_price;
+    private String Duration;
+    private String status;
+
+}
