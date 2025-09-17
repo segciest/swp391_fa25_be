4a53ce5860dc4774c4acc8ba5dad2a3f0ca698d3
khoahd14805 <huynhdangkhoaj7@gmail.com>
2025-09-17 09:27:49 +0700

    update code moi co them jwt

--- src/main/java/org/grp8/swp391/config/JwtProprties.java
+++ src/main/java/org/grp8/swp391/config/JwtProprties.java
@@ -4,11 +4,13 @@ package org.grp8.swp391.config;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@ConfigurationProperties(prefix = "jwt")
 public class JwtProprties {
     String secret;
     Long expiration;
