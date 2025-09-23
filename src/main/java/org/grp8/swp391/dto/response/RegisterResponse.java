f751bfdaa99d584bdd4edceec6dcd49b82377f63
khoahd14805 <huynhdangkhoaj7@gmail.com>
2025-09-23 14:03:14 +0700

    update code moi chinh them passwordencode

---
+++ src/main/java/org/grp8/swp391/dto/response/RegisterResponse.java
@@ -0,0 +1,22 @@
+package org.grp8.swp391.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.grp8.swp391.entity.Role;
+import org.grp8.swp391.entity.UserStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterResponse {
+
+        private String userId;
+        private String userName;
+        private String userEmail;
+        private String phone;
+        private Role roleName;
+        private UserStatus status;
+
+}
